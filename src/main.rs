fcd68a8209381c5d5862df8fffc42a13f631fc36
NicB <109698029+green-ogre@users.noreply.github.com>
2024-08-13 19:27:55 -0600

    added tileset

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,59 @@
-use avian2d::PhysicsPlugins;
 use bevy::{
-    diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin},
+    input::{keyboard::KeyboardInput, ButtonState},
     prelude::*,
     window::WindowResolution,
 };
-use maze::MazePlugin;
-use player::PlayerPlugin;
+use bevy_ecs_ldtk::prelude::*;
 
-pub mod animated_sprites;
-pub mod maze;
-pub mod player;
+const WINDOW_SIZE: f32 = 1000.0;
+const TILE_SIZE: f32 = 512.0;
+const TILE_MAP_SIZE: f32 = 16.0;
 
 fn main() {
-    App::default()
-        .add_plugins((
+    App::new()
+        .add_plugins(
             DefaultPlugins
+                .set(ImagePlugin::default_nearest())
                 .set(WindowPlugin {
                     primary_window: Some(Window {
-                        title: String::from("maze-lite"),
-                        resolution: WindowResolution::new(1200.0, 1200.0),
+                        resolution: WindowResolution::new(WINDOW_SIZE, WINDOW_SIZE),
                         ..Default::default()
                     }),
-                    ..default()
-                })
-                // for crisp sprites if we want that
-                .set(ImagePlugin::default_nearest()),
-            MazePlugin,
-            PhysicsPlugins::default(),
-            PlayerPlugin,
-            FrameTimeDiagnosticsPlugin,
-            LogDiagnosticsPlugin::default(),
-        ))
-        .insert_resource(avian2d::prelude::Gravity::ZERO)
+                    ..Default::default()
+                }),
+        )
+        .add_plugins(LdtkPlugin)
         .add_systems(Startup, setup)
+        .add_systems(Update, close_on_escape)
+        .insert_resource(LevelSelection::index(0))
         .run();
 }
 
-fn setup(mut commands: Commands) {
-    commands.spawn(Camera2dBundle::default());
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    let mut camera = Camera2dBundle::default();
+    camera.projection.scale = (TILE_SIZE * TILE_MAP_SIZE / 2.0) / (WINDOW_SIZE);
+    camera.transform.translation.x = WINDOW_SIZE / 2.0 * camera.projection.scale;
+    camera.transform.translation.y = WINDOW_SIZE / 2.0 * camera.projection.scale;
+    commands.spawn(camera);
+
+    commands.spawn(LdtkWorldBundle {
+        ldtk_handle: asset_server.load("map.ldtk"),
+        ..Default::default()
+    });
+}
+
+fn close_on_escape(mut input: EventReader<KeyboardInput>, mut writer: EventWriter<AppExit>) {
+    for e in input.read() {
+        if matches!(e, KeyboardInput {
+
+            key_code,
+            state,
+            ..
+        }
+
+            if *key_code == KeyCode::Escape && *state == ButtonState::Pressed
+        ) {
+            writer.send(AppExit::Success);
+        }
+    }
 }
